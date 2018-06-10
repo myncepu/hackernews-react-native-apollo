fd2246ae4a1dd97c2d2efb9450476b3a99482b80
Yan Pengqiang <yan2010@live.com>
2018-06-11 00:19:09 +0800

    add TabNavigator

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
-import LinkList from './screens/linkList'
+import AppNavigator from './navigator'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <LinkList />
-      </View>
+      <AppNavigator />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ src/navigator.js
@@ -0,0 +1,37 @@
+/*
+ * navigator.js
+ * Copyright (C) 2018 yanpengqiang <yan2010@live.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+import React from 'react'
+import { createBottomTabNavigator } from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import LinkList from './screens/linkList'
+import CreateLink from './screens/createLink'
+
+export default createBottomTabNavigator({
+  LinkList,
+  CreateLink,
+}, {
+  navigationOptions: ({navigation}) => ({
+    tabBarIcon: ({focused, tintColor}) => {
+      const { routeName } = navigation.state
+      let iconName
+      switch (routeName) {
+        case 'LinkList':
+          iconName = `ios-home${focused ? '' : '-outline'}`
+          break
+        case 'CreateLink':
+          iconName = `ios-create${focused ? '' : '-outline'}`
+          break
+      }
+      return <Ionicons name={iconName} size={25} color={tintColor} />
+    },
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }
+  })
+})
