067053c33309db8c0334fa0871ee8a88bca0420a
Yan Pengqiang <yan2010@live.com>
2018-06-10 19:33:08 +0800

    wrap the App component with ApolloClient

--- App.js
+++ App.js
@@ -1,3 +1,26 @@
+import React, {Component} from 'react'
+import {ApolloProvider} from 'react-apollo'
+import {ApolloClient} from 'apollo-client'
+import {HttpLink} from 'apollo-link-http'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+
 import App from './src'
 
-export default App
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+export default class ApolloApp extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client} >
+        <App />
+      </ApolloProvider>
+    )
+  }
+}
