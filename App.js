32ae95af4c9095d841fc1c94fee5d00ba8c1d6ae
Yan Pengqiang <yan2010@live.com>
2018-06-11 23:38:53 +0800

    if token exists in AsyncStorage, add it to headers

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import {AsyncStorage} from 'react-native'
 import {ApolloProvider} from 'react-apollo'
 import {ApolloClient} from 'apollo-client'
 import {HttpLink} from 'apollo-link-http'
 import {InMemoryCache} from 'apollo-cache-inmemory'
-import {ApolloLink} from 'apollo-client-preset'
+import {from} from 'apollo-client-preset'
+import { setContext } from 'apollo-link-context'
 
 import {AUTH_TOKEN} from './src/constants'
 
@@ -13,8 +15,22 @@ const httpLink = new HttpLink({
   uri: 'http://localhost:4000'
 })
 
+
+// 异步方式设置header
+const authMiddleware = setContext(async (req, { headers }) => {
+  const token = await AsyncStorage.getItem(AUTH_TOKEN)
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    },
+  }
+})
+
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: from([authMiddleware, httpLink]),
   cache: new InMemoryCache(),
 })
 
