3e7ab6dadf208c0530a183de980e5a212bfa46bd
Yan Pengqiang <yan2010@live.com>
2018-06-11 23:39:55 +0800

    authLoading screen

---
+++ src/screens/authLoading.js
@@ -0,0 +1,48 @@
+/*
+ * authLoading.js
+ * Copyright (C) 2018 yanpengqiang <yan2010@live.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+import React from 'react'
+import {
+  View,
+  ActivityIndicator,
+  StatusBar,
+  StyleSheet,
+  AsyncStorage,
+} from 'react-native'
+
+export default class AuthLoadingScreen extends React.Component {
+  constructor() {
+    super()
+    this._bootstrapAsync()
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken')
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth')
+  };
+
+  // Render any loading content that you like here
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
